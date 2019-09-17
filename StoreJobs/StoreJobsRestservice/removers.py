de35920376df688bcb79de6f47a8dc8dfa9cbfd4
Anusha Upputholla <Inthyd15523aupp1@cogenthyd.com>
2019-09-17 11:32:45 +0530

    lkjsfv

--- StoreJobs/StoreJobsRestservice/removers.py
+++ StoreJobs/StoreJobsRestservice/removers.py
@@ -298,7 +298,7 @@ def replacer(data):
     return data
 def HtmlParser(data,job={}):
     items=  ('Date Posted:','Deadline','Salary','Deadline:','Salary:','location:','locations:','work location(s):','team:', 'reports to:','title:','hours:','pay rate:','Req. ID:','Recruiter:','Role:','Position Location:','Reports To:','Allocation Specialist','Business Unit:','Supervision:','Supervision:','Full Time, Fixed Term - 12 Months','Requisition ID:','Position Title:','Project:','Relocation Authorized:','Position to be Panel Interviewed?','Grade:','Work Authorization:','Other Requirements:','Company:','Req ID:','Date:','Start Date:','Work type:','Categories:','Job no:','Contract:','Profile :','Scope :','DEPARTMENT:','BASE RATE OF PAY:','SHIFT:','Your future manager :','Scope :','Reporting Relationship','Employee Status:','Work Location:','Role Location:','Role Type:','Shift Schedule:','Rostered Hours:','Hours and shift type','Job Family:','Supervisor:',"Role:",'Permanent Position','Schedule:','Audition Date & Time:','permanent position','Posting Number:','Position Type:','Classification:','Status:','Department:','Hours:','Reports to:')
-    items_starts_with=('POSITION:','Location:','Department:','Temporary position (1 year)','Bass (1 year appointment)')
+    items_starts_with=('POSITION:','Location:','Department:','Temporary position (1 year)','Bass (1 year appointment)','Position:','Shift:')
     soup=BeautifulSoup(data,"html.parser")
     datarefineer=('primary_location',
                 'recruiter_name',
