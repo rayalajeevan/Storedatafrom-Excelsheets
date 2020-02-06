848017ca3b14e63040bd922fbac11e583fbe7017
Anusha Upputholla <Inthyd15523aupp1@cogenthyd.com>
2020-02-06 17:40:35 +0530

    agskskn

--- StoreJobs/StoreJobsRestservice/removers.py
+++ StoreJobs/StoreJobsRestservice/removers.py
@@ -803,7 +803,7 @@ def refining_job(job):
                 job =Instructions(obj.instruction_id,job).method_caller()
             else:
                 query={}
-                for column_name in ('html_tags','attrs','keywords','apply_link',"ul_li_tags"):
+                for column_name in ('html_tags','attrs','keywords','apply_link','ul_li_tags'):
                     if (column_name=='attrs' or column_name=='ul_li_tags') and obj.__dict__.get(column_name)!=None:
                         query[column_name]=json.loads(obj.__dict__.get(column_name))
                         continue
@@ -814,9 +814,8 @@ def refining_job(job):
 
     #identifying the internship or jobs
     type=None
-    for value in (job['job_title'],type_job,job.get('functional_area')):
+    for value in (job['job_title'],job.get('job_type'),job.get('functional_area')):
         value=str(value).strip().replace('-',' ')
-
         value=value.replace(',',' ').replace('/',' ').replace(":",' ').replace(';',' ').replace('(',' ').replace(')',' ').replace('@',' ')
         for identifiers in ('intern','intern.','intern,','intern ','internships-','internships','internships.','internships,','internships ','internship-','internship','internship.','internship,','internship ','fellowship','fellowship.','fellowship,','fellowship ','fellowships','fellowships.','fellowships,','fellowships ','aperentship','aperentship.','aperentship,','aperentship ','trainee','trainee.','trainee,','trainee ','apprenticeship','apprenticeship.','apprenticeship,','apprenticeship ','aperentships','aperentships.','aperentships,','aperentships '):
             if identifiers in [str(x).lower().strip() for x in value.split()] :
